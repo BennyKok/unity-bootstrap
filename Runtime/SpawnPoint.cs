c95304ad03c5095d3c31d6b25e6c23e962b70300
BennyKok <itechbenny@gmail.com>
2021-03-13 14:05:46 +0800

    disableEnableCinemachine options for spawn point To prevent cinemachine from moving too fast when teleporing

--- Runtime/SpawnPoint.cs
+++ Runtime/SpawnPoint.cs
@@ -13,8 +13,17 @@ namespace BennyKok.Bootstrap
         public int buildSpawnPoint;
         private GameObject player;
 
+#if HAS_CINEMACHINE
+        [Tooltip("To prevent cinemachine from moving too fast when teleporing")] public bool disableEnableCinemachine;
+        private Cinemachine.CinemachineBrain brain;
+#endif
+
         void Start()
         {
+#if HAS_CINEMACHINE
+            brain = Camera.main.gameObject.GetComponent<Cinemachine.CinemachineBrain>();
+#endif
+
             player = GameObject.FindGameObjectWithTag("Player");
             if (!Application.isEditor)
             {
@@ -59,9 +68,24 @@ namespace BennyKok.Bootstrap
 
         IEnumerator MoveCoroutine()
         {
+#if HAS_CINEMACHINE
+            var tempVCam = brain.ActiveVirtualCamera;
+            if (disableEnableCinemachine)
+            {
+                tempVCam.VirtualCameraGameObject.SetActive(false);
+            }
+#endif
             yield return new WaitForEndOfFrame();
+
             player.transform.position = transform.GetChild(editorSpawnPoint).transform.position;
             player.transform.rotation = transform.GetChild(editorSpawnPoint).transform.rotation;
+
+#if HAS_CINEMACHINE
+            if (disableEnableCinemachine)
+            {
+                tempVCam.VirtualCameraGameObject.SetActive(true);
+            }
+#endif
         }
     }
 }
