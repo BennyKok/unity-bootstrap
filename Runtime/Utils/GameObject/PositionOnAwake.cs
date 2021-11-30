b19ef364daf18be4d7f1987a567bd3db686fe76e
BennyKok <itechbenny@gmail.com>
2021-11-30 20:36:03 +0800

    feat: add ScaleOnAwake and PositionOnAwake

---
+++ Runtime/Utils/GameObject/PositionOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace BennyKok.Bootstrap
+{
+    public class PositionOnAwake : MonoBehaviour
+    {
+        public bool setLocal = true;
+        public Vector3 position = Vector3.zero;
+
+        private void Awake()
+        {
+            if (setLocal)
+                transform.localPosition = position;
+            else
+                transform.position = position;
+        }
+    }
+}
---
+++ Runtime/Utils/GameObject/ScaleOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace BennyKok.Bootstrap
+{
+    public class ScaleOnAwake : MonoBehaviour
+    {
+        public Vector3 scale = Vector3.one;
+
+        private void Awake()
+        {
+            transform.localScale = scale;
+        }
+    }
+}
